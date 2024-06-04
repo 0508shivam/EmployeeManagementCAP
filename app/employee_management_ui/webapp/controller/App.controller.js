b3946ae61b396605d440af3c747a41a131ed6467
Shivam Tripathi <shivamtripathi@deloitte.com>
2024-06-04 12:37:52 +0000

    ToolPage UI Changes.

--- app/employee_management_ui/webapp/controller/App.controller.js
+++ app/employee_management_ui/webapp/controller/App.controller.js
@@ -1,15 +1,23 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    'sap/ui/Device'
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Device) {
         "use strict";
 
         return Controller.extend("sap.deloitte.employeemanagement.employeemanagementui.controller.App", {
             onInit: function () {
+                if (Device.resize.width <= 1024) {
+                    this.onSideNavButtonPress();
+                }
+            },
 
-            }
+            onSideNavButtonPress: function() {
+                var oToolPage = this.byId("_IDGenToolPage1");
+                oToolPage.setSideExpanded(!oToolPage.getSideExpanded());
+            },
         });
     });
