25d237665852f2166588805221787402f96cb6c7
Aviral Mukesh <avimukesh@deloitte.com>
2024-06-13 08:39:52 +0000

    object page layout and routing for UUI cards

--- app/employee_management_ui/webapp/controller/App.controller.js
+++ app/employee_management_ui/webapp/controller/App.controller.js
@@ -1,12 +1,13 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    'sap/ui/Device'
+    'sap/ui/Device',
+    "sap/ui/model/Context"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel, Device) {
+    function (Controller, JSONModel, Device, Content) {
         "use strict";
 
         return Controller.extend("sap.deloitte.employeemanagement.employeemanagementui.controller.App", {
@@ -18,8 +19,12 @@ sap.ui.define([
                 cardManifests.loadData(sap.ui.require.toUrl("sap/deloitte/employeemanagement/employeemanagementui/model/cardManifests.json"));
                 this.getView().setModel(cardManifests, "manifests");
             },
+            onAction: function(oEvent) {
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.navTo("needsObjectPage/{name}");
+            },
 
-            onSideNavButtonPress: function() {
+            onSideNavButtonPress: function(oEvent) {
                 var oToolPage = this.byId("_IDGenToolPage1");
                 oToolPage.setSideExpanded(!oToolPage.getSideExpanded());
             },
