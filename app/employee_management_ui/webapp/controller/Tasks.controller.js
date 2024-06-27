a43c20583863490573921dc8f8240e9a9a251e54
Shivam Tripathi <shivamtripathi@deloitte.com>
2024-06-27 14:24:05 +0000

    Added Notify Employee View

--- app/employee_management_ui/webapp/controller/Tasks.controller.js
+++ app/employee_management_ui/webapp/controller/Tasks.controller.js
@@ -77,15 +77,19 @@ function(Controller, JSONModel){
             var oModel = new JSONModel(mData);
             this.getView().setModel(oModel);
         },
+
         onItemPress: function (oEvent) {
+
             var oItem = oEvent.getSource();
             var oBindingContext = oItem.getBindingContext();
             var sTaskId = oBindingContext.getProperty("title");
 
             var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-            oRouter.navTo("Position Validation",{
+            oRouter.navTo(sTaskId,{
                 title: sTaskId
             });
+            
         }
+
     });
 });
