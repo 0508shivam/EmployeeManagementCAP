8f138a82d851caf6ad05e1ab21762dab793d760e
Aviral Mukesh <avimukesh@deloitte.com>
2024-07-22 13:02:31 +0000

    Aviral changes with i18n

--- app/employee_management_ui/webapp/controller/sharedBlocks/postPMG.js
+++ app/employee_management_ui/webapp/controller/sharedBlocks/postPMG.js
@@ -2,6 +2,12 @@ sap.ui.define(["sap/uxap/BlockBase"], function(BlockBase) {
     "use strict"
 
     return BlockBase.extend("sap.deloitte.employeemanagement.employeemanagementui.view.sharedBlocks.postPMG",{
-        metadata:{}
+        metadata:{},
+        onKickOffPress: function(oEvent) {
+            var oview =  oEvent.getSource().getId("postPMG");
+            oview.setVisible(true);
+            // var oInput = this.getView().byId("postPMG");
+            // oInput.setProperty("postPMGActivities", "true");
+        }
     });
 });
