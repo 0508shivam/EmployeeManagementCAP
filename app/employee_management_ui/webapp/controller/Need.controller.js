f0d1f77c2d1e6cef38af261d31bb97e6683cc304
Shivam Tripathi <shivamtripathi@deloitte.com>
2024-08-14 06:43:27 +0000

    Overview Screen Updated

--- app/employee_management_ui/webapp/controller/Need.controller.js
+++ app/employee_management_ui/webapp/controller/Need.controller.js
@@ -227,8 +227,8 @@ function (Controller, JSONModel) {
             var oModel = new JSONModel(mData);
             this.getView().setModel(oModel);
         },
+
         onEditNeedPress: function() {
-            console.log("Hi");
             var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
                 oRouter.navTo("Edit Need");
         },
@@ -248,6 +248,16 @@ function (Controller, JSONModel) {
                 this._oAddCandidateDialog = null;
             }
         },
+
+        onPressHome: function() {
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.navTo("home");
+        },
+
+        onPressNeeds: function() {
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.navTo("Main");
+        }
 
     });
 });
